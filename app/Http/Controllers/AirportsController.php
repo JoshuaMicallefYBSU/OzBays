12d68edc5cf94a58b1be0ee3be31ecd4cfb62d5b
JoshuaMicallefYBSU <91457812+JoshuaMicallefYBSU@users.noreply.github.com>
2025-12-30 04:27:11 +1000

    Partials Changes & Dashboard Flight Update

--- app/Http/Controllers/AirportsController.php
+++ app/Http/Controllers/AirportsController.php
@@ -28,24 +28,4 @@ class AirportsController extends Controller
         }
         return view('airport.view', compact('airport'));
     }
-
-    // Update Fucntion to rended the Ladder for the Airport
-    public function updateLadder($icao)
-    {
-        $taxing = Flights::where('arr', $icao)->where('status', 'Arrived')->where('Online', 1)->with('mapBay')->orderBy('callsign', 'asc')->get();
-
-        // return $taxing;
-
-        $arrival = Flights::where('arr', $icao)->where('status', 'On Approach')->where('Online', 1)->with('mapBay')->orderBy('distance', 'asc')->get();
-
-        $occupied_bays = Bays::where('airport', $icao)
-            ->where('status', 2)->whereIn('id', function ($q) {
-                $q->selectRaw('MIN(id)')
-                ->from('bays')
-                ->where('status', 2)
-                ->groupBy('callsign');
-            })->orderBy('callsign', 'asc')->get();
-
-        return view('airport.ladder', compact('icao', 'taxing', 'arrival', 'occupied_bays'))->render();
-    }
 }
